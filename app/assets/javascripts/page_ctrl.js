a1607d0371cae2db32b3bfef4c2ee19c233a679d
writeclubchicago1@yahoo.com <writeclubchicago1@yahoo.com>
2015-04-01 20:18:48 -0500

    hello

--- app/assets/javascripts/page_ctrl.js
+++ app/assets/javascripts/page_ctrl.js
@@ -10,18 +10,18 @@
 
 $scope.addSuperhero = function(newName) {
   var superhero = 
-  name: newName
+  {name: newName
 };
 
-$http.post('/api/v1/superheros.json', superhero).then (function(response)) {$scope.people.push(superhero);}
-function (error) {
+$http.post('/api/v1/superheros.json', superhero).then (function(response) {
+  $scope.superheros.push(superhero);
+  $scope.name = "";
 
+}, function (error) {
 });
-
-$scope.name = "";
-
+}
  $scope.toggleVisible = function(superhero) { superhero.nameVisible = !superhero.nameVisible;
-  	};
+  	}
   	window.scope = $scope;
   });
-}()); 
+})(); 
